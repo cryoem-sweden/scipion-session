58da3edfc50de57cdd3f94c6952fab358785b1b4
Jose Miguel de la Rosa Trevin <delarosatrevin@gmail.com>
2017-08-19 22:13:23 +0200

    Updated read_sessions.py script to test update

--- scripts/read_sessions.py
+++ scripts/read_sessions.py
@@ -7,5 +7,14 @@ dataFn = getDataFile(SESSIONS_FILE)
 
 sMan = SessionManager(dataFn)
 
-for session in sMan.getSessions():
+sessionsSet = sMan.getSessions()
+sessionsSet.enableAppend()
+sessions = [s.clone() for s in sessionsSet]
+
+
+for session in sessions:
     session.printAll()
+    session.microscopeSettings.set({"a": 1})
+    sessionsSet.update(session)
+
+sessionsSet.write()
